e2beb263a1af71036fa7bb409ec2693109734f98
Kendale Hamilton <kendale@carloconsultants.com>
2025-03-04 01:55:44 -0500

    feat: Implemented backwards pass

--- api/Models/Knapsack.cs
+++ api/Models/Knapsack.cs
@@ -63,6 +63,12 @@ namespace Models.Knapsack
             _values = values.Select(x => new Complex(x, 0)).ToArray();
         }
 
+        public Vec(int size, double value)
+        {
+            _values = new Complex[size + 1];
+            _values[size] = new Complex(value, 0);
+        }
+
         public Complex this[int index]
         {
             get => _values[index];
@@ -81,15 +87,8 @@ namespace Models.Knapsack
 
         public void Print(string prepend = "")
         {
-            List<double> nonzeroIndexes = new List<double>();
-            for (int i = 0; i < _values.Length; i++)
-            {
-                if (_values[i].Magnitude > .1)
-                {
-                    nonzeroIndexes.Add(i);
-                }
-            }
-            Console.WriteLine(prepend + string.Join(", ", nonzeroIndexes));
+            List<int> nonZeroIndexes = GetNonZeroIndexes();
+            Console.WriteLine(prepend + string.Join(", ", nonZeroIndexes));
         }
 
         public void PrintReal(string prepend = "")
@@ -97,7 +96,15 @@ namespace Models.Knapsack
             List<double> reals = new List<double>();
             for (int i = 0; i < _values.Length; i++)
             {
-                reals.Add(_values[i].Real);
+                double value = _values[i].Real;
+                if (value > .1 || value < -.1)
+                {
+                    reals.Add(value);
+                }
+                else 
+                {
+                    reals.Add(0);
+                }
             }
             Console.WriteLine(prepend + string.Join(", ", reals));
         }
@@ -136,6 +143,105 @@ namespace Models.Knapsack
             }
             return Slice(0, i + 1);
         }
+
+        public void Reverse()
+        {
+            Array.Reverse(_values);
+        }
+
+        public Vec Shift(int amount)
+        {
+            // Print("Pre Shifting Original:");
+            // PrintReal("Pre Shifting Original:");
+            // Console.WriteLine($"Shifting by {amount}");
+            Vec shifted = new Vec(_values.Length - amount);
+            // shifted.PrintReal("Pre Shifting Shifted:");
+            for (int i = 0; i < shifted.Length; i++)
+            {
+                shifted[i] = _values[i + amount];
+            }
+            // shifted.PrintReal("Post Shifting Shifted:");
+            return shifted;
+        }
+        public int Min()
+        {
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if (_values[i].Real > .1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public List<int> GetNonZeroIndexes()
+        {
+            List<int> nonzeroIndexes = new List<int>();
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if (_values[i].Real > .1)
+                {
+                    nonzeroIndexes.Add(i);
+                }
+            }
+            return nonzeroIndexes;
+        }
+
+        public bool ContainsValue(int value)
+        {
+            if (_values[value].Real > .1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int FirstMatch(Vec vector)
+        {
+            List<int> selfIndexes = GetNonZeroIndexes();
+            List<int> otherIndexes = vector.GetNonZeroIndexes();
+            foreach (int selfIndex in selfIndexes)
+            {
+                foreach (int otherIndex in otherIndexes)
+                {
+                    if (selfIndex == otherIndex)
+                    {
+                        return selfIndex;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public Vec DeepCopy()
+        {
+            Vec copy = new Vec(Length);
+            for (int i = 0; i < Length; i++)
+            {
+                copy[i] = _values[i];
+            }
+            return copy;
+        }
     }
+
+    public class SubsetNode
+    {
+        public Vec Vector;
+        public SubsetNode Parent;
+        public SubsetNode LeftChild;
+        public SubsetNode RightChild;
+        public SubsetNode(Vec vec)
+        {
+            Vector = vec;
+        }
+        public SubsetNode(Vec vec, SubsetNode left, SubsetNode right)
+        {
+            Vector = vec;
+            LeftChild = left;
+            RightChild = right;
+        }
+    }
+
     #endregion
 }
--- api/Services/KnapsackService.cs
+++ api/Services/KnapsackService.cs
@@ -8,43 +8,66 @@ namespace Services.KnapsackService
         public KnapsackService(){}
         public Task<List<Track>> SolveKnapsack(int length, List<Track> tracks)
         {
+
             double[] values = new double[tracks.Count];
             for (int i = 0; i < tracks.Count; i++)
             {
                 values[i] = (double)tracks[i].DurationMs;
             }
 
-            Vec[] vectors = new Vec[tracks.Count];
-
+            SubsetNode[] vectors = new SubsetNode[tracks.Count];
             for (int i = 0; i < tracks.Count; i++)
             {
                 int size = (int)tracks[i].DurationMs;
                 Vec vector = new Vec(size + 1);
                 vector[0] = 1;
                 vector[size] = 1;
-                vectors[i] = vector;
+                vectors[i] = new SubsetNode(vector);
             }
+
             
             Console.WriteLine("Starting FFT Convolution");
-            while (vectors.Length > 1)
+            SubsetNode[] level = vectors;
+            while (level.Length > 1)
             {
-                for (int i = 0, j = 0; j < vectors.Length - 1; i += 1, j += 2)
+                SubsetNode[] nextLevel = new SubsetNode[(level.Length + 1) / 2];
+                for (int i = 0, j = 0; j < level.Length - 1; i+=1, j+=2)
                 {
-                    if (j + 1 < vectors.Length)
+                    if (j + 1 < level.Length)
                     {
-                        vectors[i] = FFTConvolve(vectors[j], vectors[j + 1]);
+                        SubsetNode left = level[j];
+                        SubsetNode right = level[j + 1];
+                        Vec sum = FFTConvolve(left.Vector, right.Vector);
+                        SubsetNode node = new SubsetNode(sum, left, right);
+                        nextLevel[i] = node;
                     }
                     else
                     {
-                        vectors[i] = vectors[j];
+                        SubsetNode child = level[j];
+                        nextLevel[i] = child;
                     }
                 }
-                vectors = new ArraySegment<Vec>(vectors, 0, (vectors.Length + 1) / 2).ToArray();
-            }
-            Vec result = vectors[0];
+                level = nextLevel;
+            }   
+
             Console.WriteLine("Finished FFT Convolution");
-            
-            result.Print("Final FFT Result: ");
+
+            Console.WriteLine("Starting Backwards Pass");
+            SubsetNode top = level[0];
+
+            if (!top.Vector.ContainsValue(length))
+            {
+                Console.WriteLine("No solution found");
+                return Task.FromResult(new List<Track>());
+            }
+            Vec total = new Vec(length, 1);
+
+            List<Vec> selections = BackwardsPass(total, top.LeftChild, top.RightChild);
+            Console.WriteLine("Finished Backwards Pass");
+            foreach (Vec selection in selections)
+            {
+                selection.Print("Selection: ");
+            }
 
             return Task.FromResult(tracks);
         }
@@ -78,6 +101,16 @@ namespace Services.KnapsackService
             }
         }
 
+        private Vec VectorSubtraction(Vec lhs, Vec rhs)
+        {
+            // Console.WriteLine("Subtracting vectors");
+            Vec reversed = rhs.DeepCopy();
+            reversed.Reverse();
+            Vec convolve = FFTConvolve(lhs, reversed);
+            int shiftValue = rhs.Length - 1; 
+            return convolve.Shift(shiftValue);
+        }
+
         private Vec FFTConvolve(Vec lhs, Vec rhs)
         {
             // Console.WriteLine($"Convolving lhs: {string.Join(", ", lhs)} and rhs: {string.Join(", ", rhs)}");
@@ -105,6 +138,26 @@ namespace Services.KnapsackService
             FFT(result);
             Vec resultSegment = result.Slice(0, 2 * n - 1);
             return resultSegment;
+        }
+        private List<Vec> BackwardsPass(Vec sum, SubsetNode left, SubsetNode right)
+        {
+            if (left == null && right == null)
+            {
+                return [sum];
+            }
+            Vec leftoverLeft = VectorSubtraction(sum, right.Vector);
+            Vec leftoverRight = VectorSubtraction(sum, left.Vector);
+
+            List<Vec> leftPass;
+            List<Vec> rightPass;
+            int lMatch = left.Vector.FirstMatch(leftoverLeft);
+            Vec lMatchVec = new Vec(lMatch, 1);
+            leftPass = BackwardsPass(lMatchVec, left.LeftChild, left.RightChild);
+            int rMatch = right.Vector.FirstMatch(leftoverRight);
+            Vec rMatchVec = new Vec(rMatch, 1);
+            rightPass = BackwardsPass(rMatchVec, right.LeftChild, right.RightChild);
+            List<Vec> children = [.. leftPass, .. rightPass];
+            return children;
         }
 
         private double MaxConvolution(int lhs, int rhs)
