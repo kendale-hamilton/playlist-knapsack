deba8028bfabde829362acbdd98170defed32996
Kendale Hamilton <kendale@carloconsultants.com>
2025-04-04 17:30:29 -0400

    fix: Updated how cookies work in preview

--- next-app/src/app/helpers/cookie-functions.ts
+++ next-app/src/app/helpers/cookie-functions.ts
@@ -2,30 +2,16 @@ import { Cookies } from "@/types/cookies";
 import { deleteCookie, getCookie } from "cookies-next";
 
 export default function getCookies(): Cookies {
-    const isPreview = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
-    if (!isPreview) {
-        const cookieStore: Cookies = {
-            userId: getCookie('userId') as string | undefined,
-            userDisplayName: getCookie('userDisplayName') as string | undefined,
-            userEmail: getCookie('userEmail') as string | undefined,
-            userAvatar: getCookie('userAvatar') as string | undefined,
-            accessToken: getCookie('accessToken') as string | undefined,
-            refreshToken: getCookie('refreshToken') as string | undefined,
-        };
-        return cookieStore;
-    }
-    else {
-        const cookieStore: Cookies = {
-            userId: process.env.NEXT_PUBLIC_USER_ID,
-            userDisplayName: process.env.NEXT_PUBLIC_USER_DISPLAY_NAME,
-            userEmail: process.env.NEXT_PUBLIC_USER_EMAIL,
-            userAvatar: process.env.NEXT_PUBLIC_USER_AVATAR,
-            accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-            refreshToken: process.env.NEXT_PUBLIC_REFRESH_TOKEN,
-        };
-        // console.log('cookieStore:', cookieStore);
-        return cookieStore;
-    }
+    const cookieStore: Cookies = {
+        userId: (getCookie('userId') as string | undefined) || process.env.NEXT_PUBLIC_USER_ID,
+        userDisplayName: (getCookie('userDisplayName') as string | undefined) || process.env.NEXT_PUBLIC_USER_DISPLAY_NAME,
+        userEmail: (getCookie('userEmail') as string | undefined) || process.env.NEXT_PUBLIC_USER_EMAIL,
+        userAvatar: (getCookie('userAvatar') as string | undefined) || process.env.NEXT_PUBLIC_USER_AVATAR,
+        accessToken: (getCookie('accessToken') as string | undefined) || process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+        refreshToken: (getCookie('refreshToken') as string | undefined) || process.env.NEXT_PUBLIC_REFRESH_TOKEN,
+    };
+
+    return cookieStore;
 }
 
 export function clearCookies() {
