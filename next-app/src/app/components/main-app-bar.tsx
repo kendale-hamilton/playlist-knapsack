55dc7eb0efdbb6a5e43c77332c2467e1c1ad5962
Kendale Hamilton <kendale@carloconsultants.com>
2025-04-04 17:21:50 -0400

    delete: Removed unused scope

--- next-app/src/app/components/main-app-bar.tsx
+++ next-app/src/app/components/main-app-bar.tsx
@@ -13,7 +13,7 @@ export const signIn = async () => {
         querystring.stringify({
             response_type: 'code',
             client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-            scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-read-private user-read-email ugc-image-upload',
+            scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-read-email ugc-image-upload',
             redirect_uri: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
             state: state,
             show_dialog: true
